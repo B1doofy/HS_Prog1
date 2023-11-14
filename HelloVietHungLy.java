d0bbdc97e279b8e5ac72d3ad8d5cf2ef16ef1067
B1doofyFLY <73217371+B1doofy@users.noreply.github.com>
2023-11-14 12:46:39 +0100

    Add files via upload

---
+++ HelloVietHungLy.java
@@ -0,0 +1,8 @@
+public class HelloVietHungLy {
+	public static void main(String[] args) {
+		// gebe Name aus
+		System.out.println("Hallo Tutorinnen und Tutoren! \nIch wurde von Viet Hung Lý programmiert.");
+		boolean test = (1 !=2 ^false) && true||true;
+		System.out.println(test);
+	}
+}
